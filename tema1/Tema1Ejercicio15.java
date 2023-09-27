38c13f9bf7dc6e3476914e4a180a9b8a5a3bd23e
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2023-09-27 14:01:05 +0200

    prueba 15

---
+++ tema1/Tema1Ejercicio15.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Tema1Ejercicio15 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int month = in.nextInt();
+        int year = in.nextInt();
+        if (year % 4 == 0 && !(year % 100 == 0)) {
+        }
+    }
+}
