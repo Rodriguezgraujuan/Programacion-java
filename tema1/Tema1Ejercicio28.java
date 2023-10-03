91c5657044d5367b71ffab82aaeb229b851b062e
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2023-10-03 11:40:45 +0200

    prueba 28

--- tema1/Tema1Ejercicio28.java
+++ tema1/Tema1Ejercicio28.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Tema1Ejercicio28 {
     public static void main(String[] args){
-        Math.floor(Math.random() * (99999 + 1));
+        Scanner in = new Scanner(System.in);
+        int lot = (int) (Math.random() * (99999 + 1));
+        System.out.println(lot);
+
+        for (int i=1; i<=5; i++){
+            System.out.println("Introduce el numero " + i);
+            int num = in.nextInt();
+
+            if (num == lot){
+                System.out.println("Has hacertado");
+                break;
+            } else if (i != 5){
+                System.out.println("Vuelve a intentarlo");
+            }
+        }
     }
 }
