b490fc1f4b6feef08ec589471984bbf9c0e91aab
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2023-10-03 10:45:37 +0200

    prueba 27

---
+++ tema1/Tema1Ejercicio27.java
@@ -0,0 +1,28 @@
+import java.util.List;
+import java.util.Stack;
+
+public class Tema1Ejercicio27 {
+    public static void main(String[] args) {
+        int n = 1;
+        int primos = 0;
+        while (primos < 20) {
+            int bucle =0;
+            for (int i = 1; i<=n ; i++){
+                if (n%i==0) {
+                    bucle++;
+
+                }
+            }
+            if (bucle>2){
+            } else {
+                System.out.println(n);
+                primos++;
+            }
+            n++;
+        }
+
+
+
+
+    }
+}
