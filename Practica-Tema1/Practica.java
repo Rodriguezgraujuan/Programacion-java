7ce1f21ffd6f5e2919ea441f491d911606e8ed04
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2023-10-04 14:07:09 +0200

    Practica prueba2

--- Practica-Tema1/Practica.java
+++ Practica-Tema1/Practica.java
@@ -6,12 +6,12 @@ public class Practica {
         int velocidad1;
         int ataque1;
         int defensa1;
-        int vida1;
+        int vida1 = 0;
 
         int velocidad2;
         int ataque2;
         int defensa2;
-        int vida2;
+        int vida2 = 0;
 
         int total1 = 501;
         int total2 = 501;
@@ -27,7 +27,7 @@ public class Practica {
             System.out.println("Vida:");
             vida1 = in.nextInt();
             total1 = velocidad1 + ataque1 + defensa1 + vida1;
-            if (total1 > 500) {
+            if (total1 > 500 && velocidad1 <=200 && ataque1 <= 200 && defensa1 <=200 && vida1 <= 200) {
                 System.out.println("Atributos incorrectos. Deben ser 500 en total o menos");
             }
         }
@@ -44,8 +44,16 @@ public class Practica {
             vida2 = in.nextInt();
             total2 = velocidad2 + ataque2 + defensa2 + vida2;
 
-            if (total2 > 500) {
+            if (total2 > 500 && velocidad2 <=200 && ataque2 <= 200 && defensa2 <=200 && vida2 <= 200) {
                 System.out.println("Atributos incorrectos. Deben ser 500 en total o menos");
+            }
+        }
+        int prioridad = 0;
+        while (vida1 > 0 && vida2 > 0){
+            if (velocidad1 > velocidad2){
+                prioridad = 1;
+            } else {
+                prioridad = 2;
             }
         }
     }
