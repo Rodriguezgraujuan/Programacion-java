c84c8ebd9af477075f12a789f717e91bc2a5959a
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2023-10-05 10:49:34 +0200

    Practica prueba4

--- Practica-Tema1/Practica.java
+++ Practica-Tema1/Practica.java
@@ -1,8 +1,11 @@
+import javax.print.attribute.standard.PrinterMakeAndModel;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Practica {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        Random random = new Random();
         int velocidad1 = 0;
         int ataque1 = 0;
         int defensa1 = 0;
@@ -27,10 +30,17 @@ public class Practica {
             System.out.println("Vida:");
             vida1 = in.nextInt();
             total1 = velocidad1 + ataque1 + defensa1 + vida1;
+
             if (total1 > 500 && velocidad1 <=200 && ataque1 <= 200 && defensa1 <=200 && vida1 <= 200) {
                 System.out.println("Atributos incorrectos. Deben ser 500 en total o menos");
             }
         }
+            if (ataque1<10){
+                ataque1=10;
+            } else if (defensa1<10) {
+                defensa1=10;
+            }
+            vida1 = vida1 + 100;
 
         while (total2 > 500) {
             System.out.println("Jugador 2, Introduce tus puntos de atributos. (No pases de 500)");
@@ -41,25 +51,98 @@ public class Practica {
             System.out.println("Defensa:");
             defensa2 = in.nextInt();
             System.out.println("Vida:");
-            vida2 = in.nextInt();
+            vida2 =  in.nextInt();
             total2 = velocidad2 + ataque2 + defensa2 + vida2;
 
             if (total2 > 500 && velocidad2 <=200 && ataque2 <= 200 && defensa2 <=200 && vida2 <= 200) {
                 System.out.println("Atributos incorrectos. Deben ser 500 en total o menos");
             }
         }
+            if (ataque2<10){
+                ataque2=10;
+            } else if (defensa2<10) {
+                defensa2=10;
+            }
+           vida2 = vida2 + 100;
+
         int prioridad = 0;
         if (velocidad1 > velocidad2){
             prioridad = 1;
-        } else {
+        } else if (velocidad2>velocidad1){
             prioridad = 2;
+        } else {
+            prioridad = random.nextInt(1,2);
         }
-        while (vida1 > 0 && vida2 > 0){
-            if (prioridad==1){
+        System.out.println(prioridad);
+        int turno = 1;
+        char condicion = 's';
+        while (vida1 > 0 && vida2 > 0 && condicion=='s'){
+            System.out.println("Ronda " + turno);
+            if (prioridad==1) {
+                int hit = random.nextInt(0, ataque1 / 10);
 
-            } else {
+                if (hit<0){
 
+                }else {
+                    hit = hit - random.nextInt(0, defensa2 / 10);
+                    if (vida2 - hit < 0) {
+                        break;
+                    } else {
+                        vida2 = vida2 - hit;
+                    }
+                }
+                hit = 0;
+                hit = random.nextInt(0, ataque2 / 10);
+                hit = hit - random.nextInt(0, defensa1 / 10);
+                if (hit < 0) {
+
+                } else {
+                    if (vida1 - hit < 0){
+                        break;
+                    }else {
+                        vida1 = vida1 - hit;
+                    }
+                }
+                System.out.println("Vida del jugador 1: " + vida1);
+                System.out.println("Vida del jugador 2: " + vida2);
+                System.out.println("Quieres continuar? Pon S");
+                condicion = in.next().charAt(0);
+            } else if (prioridad==2){
+                System.out.println("Prioridad2");
+                int hit = random.nextInt(0, ataque2 / 10);
+                hit = hit - random.nextInt(0, defensa1 / 10);
+                if (hit<0){
+
+                }else {
+                    if (vida1-hit<0){
+                        break;
+                    }else {
+                        vida1 = vida1 - hit;
+                    }
+                }
+                hit = 0;
+                hit = random.nextInt(0, ataque1 / 10);
+                hit = hit - random.nextInt(0, defensa2 / 10);
+                if (hit < 0) {
+
+                } else{
+                    if (vida2-hit<0){
+                        break;
+                    } else {
+                        vida2 = vida2 - hit;
+                    }
+                }
+                System.out.println("Vida del jugador 2: " + vida2);
+                System.out.println("Vida del jugador 1: " + vida1);
+                System.out.println("Quieres continuar? Pon S");
+                condicion = in.next().charAt(0);
             }
+            turno++;
+        }
+        if (vida1 >0){
+            System.out.println("Jugador 1 gana");
+        } else {
+            System.out.println("Jugador 2 gana");
         }
     }
 }
