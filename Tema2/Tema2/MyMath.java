37460a7b04065b2eef30c8cfb14c03d183a35a0b
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2023-10-24 10:30:34 +0200

    Ejercicio 1 T-2 TEST

---
+++ Tema2/Tema2/MyMath.java
@@ -0,0 +1,27 @@
+package Tema2;
+
+public class MyMath {
+    public static double squarePerimeter(double aside){
+        return aside*4;
+    }
+
+    public static double squareArea(double aside) {
+        return aside*aside;
+    }
+
+    public static double rectanglePerimeter(double base, double height){
+        return base*2+height*2;
+    }
+
+    public static double rectangleArea(double base, double height){
+        return base*height;
+    }
+
+    public static double circlePerimeter(double radius){
+        return 2*Math.PI*radius;
+    }
+
+    public static double circleArea(double radius){
+        return Math.PI* Math.pow(radius, 2);
+    }
+}
