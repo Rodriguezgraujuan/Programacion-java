407e3feb90ea9a31df5435ef1f59bc23e67db58e
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2023-10-25 13:36:36 +0200

    MyMath 1-9

--- Tema2/Tema2/MyMath.java
+++ Tema2/Tema2/MyMath.java
@@ -1,31 +1,159 @@
 package Tema2;
 
 public class MyMath {
-    public static double squarePerimeter(double aside){
-        return aside*4;
+    public static double squarePerimeter(double lado){
+        double result = 0;
+        if (lado>=0){
+            result = (lado*4);
+        }
+        return result;
     }
 
-    public static double squareArea(double aside) {
-        return aside*aside;
+    public static double squareArea(double lado) {
+        double result = 0;
+        if (lado>=0){
+            result = (lado*lado);
+        }
+        return result;
     }
 
-    public static double rectanglePerimeter(double base, double height){
-        return base*2+height*2;
+    public static double rectanglePerimeter(double base, double altura){
+        double result = 0;
+        if (base>0 && altura>0){
+            result = (base*2+altura*2);
+        }
+        return result;
     }
 
-    public static double rectangleArea(double base, double height){
-        return base*height;
+    public static double rectangleArea(double base, double altura){
+        double result = 0;
+        if (base>0&&altura>0){
+            result = (base*altura);
+        }
+        return result;
     }
 
-    public static double circlePerimeter(double radius){
-        return 2*Math.PI*radius;
+    public static double circlePerimeter(double radio){
+        double result=0;
+        if (radio>0) {
+            result = 2 * Math.PI * radio;
+        }
+        return result;
     }
 
-    public static double circleArea(double radius){
-        return Math.PI* Math.pow(radius, 2);
+    public static double circleArea(double radio){
+        double result=0;
+        if (radio>0) {
+            result = Math.PI* Math.pow(radio, 2);
+        }
+        return result;
     }
 
-    public static int prime(int number){
+    public static boolean isPrime(int number){
+        boolean condicion = true;
+        if (number<2){
+            condicion = false;
+        } else {
+            for (int i = 2; i < number && condicion; i++) {
+                if (number % i == 0) {
+                    condicion = false;
+                }
+            }
+        }
+        return condicion;
+    }
+
+    public static boolean isNotPrime(int number){
+        return !MyMath.isPrime(number);
+    }
+
+    public static int figureCount(int number){
+        int contador = 0;
+        number = Math.abs(number);
+        if (number==0){
+            contador=1;
+        }
+        while (number>0){
+            number= number / 10;
+            contador++;
+        }
+        return contador;
+    }
+
+    public static int evenFigureCount(int number){
+        int contador=0;
+        if (number==0){
+            contador=1;
+        }
+        while (number!=0){
+            if (number%2==0){
+                contador++;
+            }
+            number= number / 10;
+        }
+        return contador;
+    }
+
+    public static int oddFigureCount(int number){
+        int contador=0;
+        number=Math.abs(number);
+        while (number!=0){
+            if (number%2==1){
+                contador++;
+            }
+            number= number / 10;
+        }
+
+        return contador;
+    }
+
+    public static int factorial(int number){
+        int result= 1;
+        if (number<0){
+            result=0;
+        }else {
+            for (int i = 1; i <= number; i++) {
+                result = i * result;
+            }
+        }
+        return result;
+    }
+
+    public static int factorialRecursive(int number){
+        int result= 1;
+        if (number<0){
+            result=0;
+        }else {
+            for (int i = 1; i < number; number--) {
+                result = number * result;
+            }
+        }
+        return result;
+    }
+
+    public static int quadraticEcuationSolutions(int A, int B, int C){
+        int result = 0;
+        int D = B*2-4*A*C;
+        if (D>0){
+            result=2;
+        } else if (D==0) {
+            result=1;
+        }
+        return result;
+    }
+
+    public static int figuresSumatory(int number){
+        number = Math.abs(number);
+        int result = 0;
+        while (number>0){
+            result += number %10;
+            number /= 10;
+        }
+        return result;
+    }
+
+    public static void main(String[] args){
 
     }
+
 }
--- Test/MyMathTest.java
+++ Test/MyMathTest.java
@@ -1,4 +1,4 @@
-package Tema2;
+import Tema2.MyMath;
 
 import org.junit.jupiter.api.Test;
 
@@ -7,54 +7,56 @@ import static org.junit.jupiter.api.Assertions.*;
 class MyMathTest {
     @Test
     void squarePerimeterTest() {
-        assertEquals( 40.4, MyMath.squarePerimeter(10.1), 0.1);
-        assertEquals( 0, MyMath.squarePerimeter(0), 0.1);
-        assertEquals( 0, MyMath.squarePerimeter(-1), 0.1, "Un lado negativo devuelve perímetro 0");
-        assertEquals( 40.004, MyMath.squarePerimeter(10.001), 0.000000001, "Precisión insuficiente");
+        assertEquals(40.4, MyMath.squarePerimeter(10.1), 0.1);
+        assertEquals(0, MyMath.squarePerimeter(0), 0.1);
+        assertEquals(0, MyMath.squarePerimeter(-1), 0.1, "Un lado negativo devuelve perímetro 0");
+        assertEquals(40.004, MyMath.squarePerimeter(10.001), 0.000000001, "Precisión insuficiente");
     }
 
     @Test
     void squareAreaTest() {
-        assertEquals( 102.01, MyMath.squareArea(10.1), 0.1);
-        assertEquals( 0, MyMath.squareArea(0), 0.1);
-        assertEquals( 0, MyMath.squareArea(-1), 0.1, "Un lado negativo devuelve área 0");
-        assertEquals( 102.01, MyMath.squareArea(10.1), 0.000000001, "Precisión insuficiente");
+        assertEquals(102.01, MyMath.squareArea(10.1), 0.1);
+        assertEquals(0, MyMath.squareArea(0), 0.1);
+        assertEquals(0, MyMath.squareArea(-1), 0.1, "Un lado negativo devuelve área 0");
+        assertEquals(102.01, MyMath.squareArea(10.1), 0.000000001, "Precisión insuficiente");
     }
 
     @Test
     void rectanglePerimeterTest() {
-        assertEquals( 40, MyMath.rectanglePerimeter(10, 10), 0.1);
-        assertEquals( 0, MyMath.rectanglePerimeter(0, 10), 0.1);
-        assertEquals( 0, MyMath.rectanglePerimeter(10, 0), 0.1);
-        assertEquals( 0, MyMath.rectanglePerimeter(-1, 10), 0.1, "Un lado negativo devuelve perímetro 0");
-        assertEquals( 0, MyMath.rectanglePerimeter(10, -1), 0.1, "Un lado negativo devuelve perímetro 0");
-        assertEquals( 40, MyMath.rectanglePerimeter(10, 10), 0.000000001, "Precisión insuficiente");
+        assertEquals(40, MyMath.rectanglePerimeter(10, 10), 0.1);
+        assertEquals(0, MyMath.rectanglePerimeter(0, 10), 0.1);
+        assertEquals(0, MyMath.rectanglePerimeter(10, 0), 0.1);
+        assertEquals(0, MyMath.rectanglePerimeter(-1, 10), 0.1, "Un lado negativo devuelve perímetro 0");
+        assertEquals(0, MyMath.rectanglePerimeter(10, -1), 0.1, "Un lado negativo devuelve perímetro 0");
+        assertEquals(40, MyMath.rectanglePerimeter(10, 10), 0.000000001, "Precisión insuficiente");
     }
 
     @Test
     void rectangleAreaTest() {
-        assertEquals( 100, MyMath.rectangleArea(10, 10), 0.1);
-        assertEquals( 0, MyMath.rectangleArea(0, 10), 0.1);
-        assertEquals( 0, MyMath.rectangleArea(10, 0), 0.1);
-        assertEquals( 0, MyMath.rectangleArea(-1, 10), 0.1, "Un lado negativo devuelve área 0");
-        assertEquals( 0, MyMath.rectangleArea(10, -10), 0.1, "Un lado negativo devuelve área 0");
-        assertEquals( 102.01, MyMath.rectangleArea(10.1, 10.1), 0.000000001, "Precisión insuficiente");
+        assertEquals(100, MyMath.rectangleArea(10, 10), 0.1);
+        assertEquals(0, MyMath.rectangleArea(0, 10), 0.1);
+        assertEquals(0, MyMath.rectangleArea(10, 0), 0.1);
+        assertEquals(0, MyMath.rectangleArea(-1, 10), 0.1, "Un lado negativo devuelve área 0");
+        assertEquals(0, MyMath.rectangleArea(10, -10), 0.1, "Un lado negativo devuelve área 0");
+        assertEquals(102.01, MyMath.rectangleArea(10.1, 10.1), 0.000000001, "Precisión insuficiente");
     }
+
     @Test
     void circlePerimeterTest() {
-        assertEquals( 62.83, MyMath.circlePerimeter(10), 0.1);
-        assertEquals( 0, MyMath.circlePerimeter(0), 0.1);
-        assertEquals( 0, MyMath.circlePerimeter(-1), 0.1, "Un radio negativo devuelve perímetro 0");
-        assertEquals( 62.8318530718, MyMath.circlePerimeter(10), 0.000000001, "Precisión insuficiente");
+        assertEquals(62.83, MyMath.circlePerimeter(10), 0.1);
+        assertEquals(0, MyMath.circlePerimeter(0), 0.1);
+        assertEquals(0, MyMath.circlePerimeter(-1), 0.1, "Un radio negativo devuelve perímetro 0");
+        assertEquals(62.8318530718, MyMath.circlePerimeter(10), 0.000000001, "Precisión insuficiente");
     }
+
     @Test
     void circleAreaTest() {
-        assertEquals( 314.16, MyMath.circleArea(10), 0.1);
-        assertEquals( 0, MyMath.circleArea(0), 0.1);
-        assertEquals( 0, MyMath.circleArea(-1), 0.1, "Un radio negativo devuelve área 0");
-        assertEquals( 314.159265359, MyMath.circleArea(10), 0.000000001, "Precisión insuficiente");
+        assertEquals(314.16, MyMath.circleArea(10), 0.1);
+        assertEquals(0, MyMath.circleArea(0), 0.1);
+        assertEquals(0, MyMath.circleArea(-1), 0.1, "Un radio negativo devuelve área 0");
+        assertEquals(314.159265359, MyMath.circleArea(10), 0.000000001, "Precisión insuficiente");
     }
-    /*
+
     @Test
     void primeTest() {
         assertFalse(MyMath.isPrime(10), "10 no es primo");
@@ -86,10 +88,11 @@ class MyMathTest {
         assertEquals(0, MyMath.evenFigureCount(1), "1 tiene 0 dígitos pares");
         assertEquals(1, MyMath.evenFigureCount(2), "2 tiene 1 dígito par");
         assertEquals(0, MyMath.evenFigureCount(999), "999 tiene 0 dígitos pares");
-        assertEquals(2, MyMath.evenFigureCount(123), "123 tiene 1 dígitos par");
+        assertEquals(1, MyMath.evenFigureCount(123), "123 tiene 1 dígitos par");
         assertEquals(1, MyMath.evenFigureCount(0), "0 tiene 1 dígito par");
         assertEquals(2, MyMath.evenFigureCount(-100), "-100 tiene 2 dígitos pares");
     }
+
     @Test
     void oddFigureCountTest() {
         assertEquals(1, MyMath.oddFigureCount(1), "1 tiene 1 dígito impar");
@@ -108,6 +111,7 @@ class MyMathTest {
         assertEquals(3628800, MyMath.factorial(10), "10! = 3628800");
         assertEquals(0, MyMath.factorial(-1), "-1! = 0, vamos a suponer que el factorial de un número negativo es 0");
     }
+
     @Test
     void factorialRecursiveTest() {
         assertEquals(2, MyMath.factorialRecursive(2), "2! = 1");
@@ -116,11 +120,12 @@ class MyMathTest {
         assertEquals(3628800, MyMath.factorialRecursive(10), "10! = 3628800");
         assertEquals(0, MyMath.factorialRecursive(-1), "-1! = 0, vamos a suponer que el factorial de un número negativo es 0");
     }
+
     @Test
     void quadraticEcuationSolutionsTest() {
-        assertEquals(2, MyMath.quadraticEcuationSolutions(-2 , 2, 1), "-2 X2 + 2 X + 1 = 0 has 2 solutions");
-        assertEquals(0, MyMath.quadraticEcuationSolutions(3 , 2, 1), "3 X2 + 2 X + 1 = 0 has 0 solutions");
-        assertEquals(1, MyMath.quadraticEcuationSolutions(1 , 2, 1), "1 X2 + 2 X + 1 = 0 has 1 solutions");
+        assertEquals(2, MyMath.quadraticEcuationSolutions(-2, 2, 1), "-2 X2 + 2 X + 1 = 0 has 2 solutions");
+        assertEquals(0, MyMath.quadraticEcuationSolutions(3, 2, 1), "3 X2 + 2 X + 1 = 0 has 0 solutions");
+        assertEquals(1, MyMath.quadraticEcuationSolutions(1, 2, 1), "1 X2 + 2 X + 1 = 0 has 1 solutions");
     }
 
     @Test
@@ -130,5 +135,4 @@ class MyMathTest {
         assertEquals(6, MyMath.figuresSumatory(123), "123 -> Todos los dígitos suman 6");
         assertEquals(6, MyMath.figuresSumatory(-123), "-123 -> Todos los dígitos suman 6");
     }
-*/
 }
