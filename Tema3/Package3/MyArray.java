a0f9dc47a1a17fc9c27eb779fd3fbee5fdcf006b
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2023-11-21 10:28:15 +0100

    Arrays ej 1

--- Tema3/Package3/MyArray.java
+++ Tema3/Package3/MyArray.java
@@ -7,14 +7,25 @@ public class MyArray {
 
     static Scanner in = new Scanner(System.in);
 
+    public static int[] creararray() {
+        System.out.println("Cuantos datos tendra la lista:");
+        int cantidadnum = in.nextInt();
+        System.out.println("Introduce los numeros:");
+        int[] array = new int[cantidadnum];
+        for (int i = 0; i < cantidadnum; i++) {
+            int num = in.nextInt();
+            array[i] = num;
+        }
+        return array;
+    }
 
-    public static void printarray(int [] Marray){
+    public static void printarray(int[] Marray) {
         for (int j : Marray) {
             System.out.println(j);
         }
     }
 
-    public static int maxarray(int [] Marray){
+    public static int maxarray(int[] Marray) {
         int max = 0;
         for (int j : Marray) {
             if (j > max) {
@@ -24,7 +35,7 @@ public class MyArray {
         return max;
     }
 
-    public static int minarray(int [] Marray){
+    public static int minarray(int[] Marray) {
         int min = maxarray(Marray);
         for (int j : Marray) {
             if (j < min) {
@@ -34,49 +45,185 @@ public class MyArray {
         return min;
     }
 
-    public static int medarray(int[] Marray){
+    public static int medarray(int[] Marray) {
         int media = 0;
-        for (int j =0; j< Marray.length; j++) {
+        for (int j = 0; j < Marray.length; j++) {
             media += Marray[j];
         }
         media /= Marray.length;
         return media;
     }
 
-    public static boolean existenumarray(int[] Marray , int numero){
+    public static boolean existenumarray(int[] Marray) {
+        System.out.println("Que numero quieres comprobar");
+        int numero = in.nextInt();
         boolean existe = false;
-        for (int j =0 ; j< Marray.length;j++) {
-            if (numero == Marray[j]){
-                existe=true;
+        for (int j = 0; j < Marray.length; j++) {
+            if (numero == Marray[j]) {
+                existe = true;
+                break;
             }
         }
         return existe;
     }
 
-    public static int[] sumvectores(int[] Marrayvect1, int[] Marrayvect2){
-        int[] a = {0,0,0,0};
-        int numvect1 = 0;
-        int numvect2 = 0;
-        for (int j = 0; j< Marrayvect1.length || j<Marrayvect2.length;j++){
-            numvect1 = Marrayvect1[j];
-            numvect2 = Marrayvect2[j];
-            a[j] = numvect1+numvect2;
+    public static int[] sumvectores(int[] Marrayvect1, int[] Marrayvect2) {
+        int cantidad = Marrayvect2.length;
+        if (Marrayvect1.length > Marrayvect2.length) {
+            cantidad = Marrayvect1.length;
+        }
+        int[] a = new int[cantidad];
+        for (int j = 0; j < Marrayvect1.length || j < Marrayvect2.length; j++) {
+            a[j] = Marrayvect1[j] + Marrayvect2[j];
         }
         return a;
     }
 
+    public static int[] restvectores(int[] Marrayvect1, int[] Marrayvect2) {
+        int cantidad = Marrayvect2.length;
+        if (Marrayvect1.length > Marrayvect2.length) {
+            cantidad = Marrayvect1.length;
+        }
+        int[] b = new int[cantidad];
+        for (int j = 0; j < Marrayvect1.length || j < Marrayvect2.length; j++) {
+            b[j] = Marrayvect1[j] - Marrayvect2[j];
+        }
+        return b;
+    }
+
+    public static int prodvectores(int[] Marrayvect1, int[] Marrayvect2) {
+        int total = 1;
+        int cantidad = Marrayvect2.length;
+        if (Marrayvect1.length > Marrayvect2.length) {
+            cantidad = Marrayvect1.length;
+        }
+        int[] c = new int[cantidad];
+        for (int j = 0; j < Marrayvect1.length || j < Marrayvect2.length; j++) {
+            c[j] = Marrayvect1[j] * Marrayvect2[j];
+            total *= c[j];
+        }
+        return total;
+    }
+
+    public static int[] invertirarray(int[] Marray) {
+        int contador = Marray.length - 1;
+        for (int i = 0; i < Marray.length / 2; i++) {
+            int num1 = Marray[i];
+            int num2 = Marray[contador];
+            Marray[i] = num2;
+            Marray[contador] = num1;
+            contador--;
+        }
+        return Marray;
+    }
+
+    public static void procedinvertarray(int[] Marray) {
+        int contador = Marray.length - 1;
+        for (int i = 0; i < Marray.length / 2; i++) {
+            int num1 = Marray[i];
+            int num2 = Marray[contador];
+            Marray[i] = num2;
+            Marray[contador] = num1;
+            contador--;
+        }
+    }
+
+    public static boolean capicua(int[]Marray) {
+        boolean condition = true;
+        int contador = Marray.length-1;
+        for (int i = 0; i < Marray.length / 2; i++) {
+            if (Marray[i]!=Marray[contador]){
+                System.out.println(Marray[i]);
+                System.out.println(Marray[contador]);
+                condition=false;
+            }
+            contador--;
+        }
+        return condition;
+    }
     public static void main(String[] args) {
-        int[] Marray = new int[]{1,2,3,7,4};
-        int[] Marrayvect1 = new int[]{9,18,27,7};
-        int[] Marrayvect2 = new int[]{19,778,457,25};
-        printarray(Marray);
-        System.out.println("Siguiente");
-        System.out.println(maxarray(Marray));
-        System.out.println(minarray(Marray));
-        System.out.println(medarray(Marray));
-        System.out.println("Que numero quieres comprobar");
-        int numero = in.nextInt();
-        System.out.println(existenumarray(Marray, numero));
-        System.out.println(Arrays.toString(sumvectores(Marrayvect1, Marrayvect2)));
+        System.out.println("¿Que quieres hacer?");
+        System.out.println("""
+                a) Un procedimiento que imprima el array.
+                b) Una función que devuelva el máximo del array.
+                c) Una función que devuelva el mínimo del array.
+                d) Una función que devuelva la media.
+                e) Una función que te diga si un elemento existe en el array o no.
+                f) Una función que haga la suma de dos vectores (arrays).
+                g) Una función que haga la resta de dos vectores (arrays).
+                h) Una función que haga el producto escalar de dos vectores
+                (arrays).
+                i) Haz una función que invierta el orden de un array.
+                j) Haz un procedimiento que invierta el orden de un array.
+                k) Haz una función que nos indique si un array es capicua o no""");
+        char eleccion = in.next().charAt(0);
+        int[] Marray;
+        int[] Marrayvect1;
+        int[] Marrayvect2;
+        switch (eleccion){
+            case 'a':
+                Marray= creararray();
+                printarray(Marray);
+                break;
+            case 'b':
+                Marray= creararray();
+                System.out.println(maxarray(Marray));
+                break;
+            case 'c':
+                Marray= creararray();
+                System.out.println(minarray(Marray));
+                break;
+            case 'd':
+                Marray= creararray();
+                System.out.println(medarray(Marray));
+                break;
+            case 'e':
+                Marray= creararray();
+                System.out.println(existenumarray(Marray));
+                break;
+            case 'f':
+                System.out.println("Primera array");
+                Marrayvect1= creararray();
+                System.out.println("Segunda array");
+                Marrayvect2= creararray();
+                if (Marrayvect1.length==Marrayvect2.length) {
+                    int[] a = sumvectores(Marrayvect1, Marrayvect2);
+                    System.out.println(Arrays.toString(a));
+                }
+                break;
+            case 'g':
+                System.out.println("Primera array");
+                Marrayvect1= creararray();
+                System.out.println("Segunda array");
+                Marrayvect2= creararray();
+                if (Marrayvect1.length==Marrayvect2.length) {
+                    int[] b = restvectores(Marrayvect1, Marrayvect2);
+                    System.out.println(Arrays.toString(b));
+                }
+                break;
+            case 'h':
+                System.out.println("Primera array");
+                Marrayvect1= creararray();
+                System.out.println("Segunda array");
+                Marrayvect2= creararray();
+                if (Marrayvect1.length==Marrayvect2.length) {
+                    System.out.println(prodvectores(Marrayvect1, Marrayvect2));
+                }
+                break;
+            case 'i':
+                Marray= creararray();
+                System.out.println(Arrays.toString(Marray));
+                System.out.println(Arrays.toString(invertirarray(Marray)));
+                break;
+            case 'j':
+                Marray= creararray();
+                System.out.println(Arrays.toString(Marray));
+                procedinvertarray(Marray);
+                System.out.println(Arrays.toString(Marray));
+                break;
+            case 'k':
+                Marray= creararray();
+                System.out.println(capicua(Marray));
+        }
     }
 }
