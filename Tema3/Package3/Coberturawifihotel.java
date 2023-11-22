1e95128120571e54a8336755e3598b6e63902c76
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2023-11-22 13:30:47 +0100

    Cobertura

--- Tema3/Package3/Coberturawifihotel.java
+++ Tema3/Package3/Coberturawifihotel.java
@@ -1,4 +1,39 @@
 package Package3;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Coberturawifihotel {
+
+    public static boolean habitacioncheck(int habitacion){
+        return habitacion < 0 || habitacion > 19;
+    }
+
+    public static int[] cobertura(int[] hotelhabitacion, int potencia, int habitacion){
+        hotelhabitacion[habitacion]=potencia;
+        int habitacion1=habitacion-1;
+        int habitacion2=habitacion+1;
+        potencia--;
+        for (int i = 0; i<potencia; potencia--){
+            if (!habitacioncheck(habitacion1)) {
+                hotelhabitacion[habitacion1] = potencia;
+            }
+            if (!habitacioncheck(habitacion2)) {
+                hotelhabitacion[habitacion2] = potencia;
+            }
+                habitacion1--;
+                habitacion2++;
+        }
+        return hotelhabitacion;
+    }
+    public static void main(String[] args) {
+        int[] hotelhabitacion = new int[20];
+        Scanner in = new Scanner(System.in);
+        System.out.println("Numero de habitación:");
+        int habitacion = in.nextInt();
+        habitacion--;
+        System.out.println("Potencia del router:");
+        int potencia = in.nextInt();
+        System.out.println(Arrays.toString(cobertura(hotelhabitacion, potencia, habitacion)));
+    }
 }
