8ef6565eebb0efbc1d23bf568fd5ebd5b61bb67d
Rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2023-11-30 19:46:39 +0100

    hoteles 1.3

--- Tema3/Package3/Coberturahotelesmatriz.java
+++ Tema3/Package3/Coberturahotelesmatriz.java
@@ -4,34 +4,155 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Coberturahotelesmatriz {
-    static int[][] hotel=new int[8][8];
+    static int[][] hotel = new int[12][5];
     static Scanner in = new Scanner(System.in);
+
     public static void printcuadradomatriz() {
         for (int[] ints : hotel) {
             System.out.println(Arrays.toString(ints));
         }
     }
 
-    public static int[][] coberturahabitacion(int piso, int habitacion, int potencia){
-        hotel[piso][habitacion]=potencia;
-        int piso1=piso;
-        int piso2=piso;
+    public static int[][] coberturahabitacion(int piso, int habitacion, int potencia) {
+        hotel[piso][habitacion] = potencia;
+        int piso1 = piso;
+        int piso2 = piso;
+        int habitacion1 = habitacion;
+        int habitacion2 = habitacion;
 
-        int potencia1=potencia;
-        int potencia2=potencia;
 
-        for (int i = 0;i<hotel.length;i++){
+        int potencia1 = potencia - 1;
+        int potencia2 = potencia - 1;
+        int potencia3 = potencia - 1;
+        int potencia4 = potencia - 1;
+
+
+        for (int i = 0; i < potencia1; potencia1--) {
             piso1--;
-            if (piso1>=0){
-                 potencia1--;
-                 hotel[piso1][habitacion]=potencia1;
-             }
+            if (piso1 >= 0) {
+                hotel[piso1][habitacion] = potencia1;
+            }
         }
-        for (int i = 0;i<hotel.length;i++){
+        for (int i = 0; i < potencia2; potencia2--) {
             piso2++;
-            if (piso2<8){
-                potencia2--;
-                hotel[piso2][habitacion]=potencia2;
+            if (piso2 <= 11) {
+                hotel[piso2][habitacion] = potencia2;
+            }
+        }
+
+        for (int i = 0; i < potencia3; potencia3--) {
+            habitacion1--;
+            if (habitacion1 >= 0) {
+                hotel[piso][habitacion1] = potencia3;
+            }
+        }
+        for (int i = 0; i < potencia4; potencia4--) {
+            habitacion2++;
+            if (habitacion2 < 5) {
+                hotel[piso][habitacion2] = potencia4;
+            }
+        }
+        int piso3=piso;
+        int potencia5=potencia;
+        int habitacion3=habitacion;
+        boolean condition=true;
+        while (condition){
+            if (habitacion3==4) {
+                condition=false;
+            } else {
+                if (potencia5 - 2<0) {
+                    potencia5 =0;
+                } else {
+                    potencia5 -= 2;
+
+                    hotel[piso3 + 1][habitacion3 + 1] = potencia5;
+                    if (hotel[piso3 + 1][habitacion3] == 1 || hotel[piso3 + 1][habitacion3] == 0 || potencia5 == 0) {
+                        hotel[piso3 + 1][habitacion3 + 1] = 0;
+
+                        piso3 = piso - 1;
+                        habitacion3++;
+                        potencia5 = hotel[piso + 1][habitacion + 1];
+                    }
+                }
+                piso3++;
+            }
+        }
+
+        potencia5=potencia;
+         piso3=piso;
+         habitacion3=habitacion;
+         condition=true;
+        while (condition){
+            if (habitacion3==0) {
+                condition=false;
+            } else {
+                if (potencia5 - 2<0) {
+                    potencia5 =0;
+                } else {
+                    potencia5-=2;
+                }
+                hotel[piso3 + 1][habitacion3 - 1] = potencia5;
+                if (hotel[piso3 + 1][habitacion3] == 1 || hotel[piso3 + 1][habitacion3] == 0 || potencia5 == 0) {
+                    hotel[piso3 + 1][habitacion3 - 1] = 0;
+
+                    piso3 =piso-1;
+                    habitacion3--;
+                    potencia5 = hotel[piso+1][habitacion+1];
+                }
+                piso3++;
+            }
+        }
+
+        potencia5=potencia;
+        piso3=piso;
+        habitacion3=habitacion;
+        condition=true;
+        while (condition){
+            if (habitacion3==0) {
+                condition=false;
+            } else {
+                if (potencia5 - 2<0) {
+                    potencia5 =0;
+                } else {
+                    potencia5-=2;
+                }
+                if (piso>0) {
+                    hotel[piso3 - 1][habitacion3 - 1] = potencia5;
+                }
+                if (hotel[piso3 - 1][habitacion3] == 1 || hotel[piso3 + 1][habitacion3] == 0 || potencia5 == 0) {
+                    hotel[piso3 - 1][habitacion3 - 1] = 0;
+
+                    piso3 =piso+1;
+                    habitacion3--;
+                    potencia5 = hotel[piso-1][habitacion-1];
+                }
+                piso3--;
+            }
+        }
+
+        potencia5=potencia;
+        piso3=piso;
+        habitacion3=habitacion;
+        condition=true;
+        while (condition){
+            if (habitacion3==4||piso==0) {
+                condition=false;
+            } else {
+                if (potencia5 - 2<0) {
+                    potencia5 =0;
+                } else {
+                    potencia5-=2;
+                }
+                    hotel[piso3 - 1][habitacion3 + 1] = potencia5;
+                    if (hotel[piso3 - 1][habitacion3] == 1 || hotel[piso3 + 1][habitacion3] == 0 || potencia5 == 0) {
+                        hotel[piso3 - 1][habitacion3 + 1] = 0;
+
+                        piso3 = piso + 1;
+                        habitacion3++;
+                        potencia5 = hotel[piso - 1][habitacion + 1];
+                    }
+
+                piso3--;
             }
         }
 
@@ -41,12 +162,12 @@ public class Coberturahotelesmatriz {
 
     public static void main(String[] args) {
         System.out.println("Introduce el piso");
-        int piso= in.nextInt();
+        int piso = in.nextInt();
         System.out.println("Introduce la habitación");
         int habitacion = in.nextInt();
         System.out.println("Introduce la potencia");
         int potencia = in.nextInt();
-        coberturahabitacion(piso,habitacion,potencia);
+        coberturahabitacion(piso, habitacion, potencia);
         printcuadradomatriz();
     }
 }
