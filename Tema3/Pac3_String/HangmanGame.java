d6c957f439421e059c1a882582d7b40d3c4f92c9
Rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2024-01-15 14:25:13 +0100

    Hangman sin borrar consola

--- Tema3/Pac3_String/HangmanGame.java
+++ Tema3/Pac3_String/HangmanGame.java
@@ -3,48 +3,78 @@ package Pac3_String;
 import java.util.Scanner;
 
 public class HangmanGame {
+    static String palabraoculta;
 
-    public static boolean estaono(String palabra,char letrabuscar){
-        boolean condition = false;
-        for (int i = 0; i<palabra.length();i++){
-            if (palabra.charAt(i)==letrabuscar){
-                condition=true;
-                i=200;
-            }
-        }
-        return condition;
+    public static void palabraocutaguiones(String palabra){
+        String guion="_";
+        palabraoculta=guion.repeat(palabra.length());
     }
 
-    public static String colocarletra(String palabra,char letrabuscar, String palabraoculta){
-        String posiciones = null;
-        for (int i = 0; i<palabra.length();i++){
-            if (palabra.charAt(i)==letrabuscar){
-                posiciones+=i;
+    public static int jugada(String palabra, String letrabuscar, int intentos){
+        boolean cambiono=false;
+        letrabuscar=letrabuscar.toLowerCase();
+        for (int i=0; i<palabra.length();i++){
+            String letrapalabra = String.valueOf(palabra.charAt(i));
+            if (letrapalabra.equals(letrabuscar)){
+                palabraoculta=palabraoculta.substring(0, i) + letrabuscar + palabraoculta.substring(i + 1);
+                cambiono=true;
             }
         }
-        for (int j = 0; j<posiciones.length();j++){
-            palabraoculta.charAt(0)=letrabuscar;
-        }
-        return palabraoculta;
-    }
-
-    public static int jugada(String palabra,char letrabuscar, int intentos){
-        String guion="-";
-        String palabraoculta=guion.repeat(palabra.length());
-        if (estaono(palabra,letrabuscar)){
-
+        if (!cambiono){
+            intentos++;
         }
         return intentos;
     }
+
+    public static boolean completaono(){
+        boolean hayguion=true;
+        for (int i =0;i<palabraoculta.length();i++){
+            String letra= String.valueOf(palabraoculta.charAt(i));
+            if (letra.equals("_")) {
+                hayguion = false;
+                break;
+            }
+        }
+        return hayguion;
+    }
+
+    public static void handman(int intentos){
+        switch (intentos){
+            case 1-> System.out.println(" o ");
+            case 2-> System.out.println(" o "+"\n"+ " | ");
+            case 3-> System.out.println(" o "+"\n"+ "/| ");
+            case 4-> System.out.println(" o "+"\n"+ "/|\\");
+            case 5-> System.out.println(" o "+"\n"+ "/|\\"+"\n"+"/");
+            case 6-> System.out.println(" o "+"\n"+ "/|\\"+"\n"+"/a \\");
+        }
+    }
+
+    public static boolean sololetras(String palabra){
+        return palabra.matches("[a-z]");
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Introduce la palabra a esconder");
-        String palabra= in.nextLine();
-        int intentos=0;
-        while (intentos<6){
+        System.out.println("Introduce la palabra:");
+        String palabra=in.nextLine();
+        palabra=palabra.toLowerCase();
+        boolean condition= true;
+        int intentos = 0;
+        palabraocutaguiones(palabra);
+        while (condition) {
             System.out.println("Introduce una letra");
-            char letrabuscar=in.next().charAt(0);
-            jugada(palabra, letrabuscar, intentos);
+            char letra = in.next().charAt(0);
+            if (sololetras(String.valueOf(letra))) {
+                intentos = jugada(palabra, String.valueOf(letra), intentos);
+                System.out.println(palabraoculta);
+                handman(intentos);
+                if (intentos > 5 || completaono()) {
+                    condition = false;
+                    System.out.println("Has ganado");
+                }
+            }else{
+                System.out.println("Introduce solo letras");
+            }
         }
     }
 }
