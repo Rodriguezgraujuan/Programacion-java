44e31d9690c3fc661824b52003575c7e4faa567e
Rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2024-01-15 15:36:33 +0100

    Lingo

--- Tema2/Tema2/comprob.java
+++ Tema2/Tema2/comprob.java
@@ -6,12 +6,6 @@ import java.util.Scanner;
 public class comprob {
 
 
-    public static void main(String[] args) {
-        System.out.printf("(+%s) - %d - %s", "34", 123, "456789");
-        Scanner in = new Scanner(System.in);
-        String a = in.nextLine();
-        String[] pepearray= a.split(" ");
-        System.out.println(Arrays.toString(pepearray));
-    }
+
 
 }
---
+++ Tema3/Pac3_String/Lingo.java
@@ -0,0 +1,70 @@
+package Pac3_String;
+
+import java.util.Scanner;
+
+public class Lingo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String palabraOculta = null;
+        System.out.println("Introduce la palabra");
+        String palabra = in.nextLine();
+        boolean condition=true;
+        while (condition) {
+            System.out.println("Intento:");
+            String palabraBuscar = in.nextLine();
+            palabraOculta = construirPalabraOculta(palabra, palabraBuscar);
+            System.out.println("Pista: " + palabraOculta);
+            if (!aciertono(palabraOculta)){
+                condition=false;
+            }
+        }
+    }
+    
+    public static boolean aciertono(String palabra){
+        boolean condition=false;
+        for (int i=0;i<palabra.length();i++){
+            String letra= String.valueOf(palabra.charAt(i));
+            if (letra.equals("-")||letra.equals("*")) {
+                condition = true;
+                break;
+            }
+        }
+        return condition;
+    }
+    public static String construirPalabraOculta(String palabra, String palabraBuscar) {
+        StringBuilder palabraOculta = new StringBuilder();
+        int longitud = Math.max(palabra.length(), palabraBuscar.length());
+
+        for (int i = 0; i < longitud; i++) {
+            char letraPalabra;
+            char letraBuscar;
+            if (i < palabra.length()) {
+                letraPalabra = palabra.charAt(i);
+            } else {
+                letraPalabra = '-';
+            }
+            if (i < palabraBuscar.length()) {
+                letraBuscar = palabraBuscar.charAt(i);
+            } else {
+                letraBuscar = '-';
+            }
+
+            if (letraPalabra == letraBuscar) {
+                palabraOculta.append(letraPalabra);
+            } else {
+                boolean estaenotraposicion=false;
+                for (int j=0;j<palabra.length();j++){
+                    if (letraBuscar==palabra.charAt(j)){
+                        palabraOculta.append('*');
+                        estaenotraposicion=true;
+                        break;
+                    }
+                }
+                if (!estaenotraposicion){
+                    palabraOculta.append('-');
+                }
+            }
+        }
+        return palabraOculta.toString();
+    }
+}
