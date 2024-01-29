8b837466ddb1158ec944e45cb3a368ac1eb52dab
Rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2024-01-29 15:30:53 +0100

    cuentas 2.2

---
+++ Tema4/POO22/CuentaBancaria.java
@@ -0,0 +1,8 @@
+package POO22;
+
+public class CuentaBancaria {
+    private String Iban; //ES1234567891113151719122
+    private String Titular;
+    private int saldo;
+    private int saldoDefecto=0;
+}
