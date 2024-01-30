0099bb0bb449dbbb6fea09d45a219b52ed1913ef
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2024-01-30 10:51:50 +0100

    CuentaBancaria 2.2

--- Tema4/POO22/CuentaBancaria.java
+++ Tema4/POO22/CuentaBancaria.java
@@ -1,8 +1,51 @@
 package POO22;
 
 public class CuentaBancaria {
-    private String Iban; //ES1234567891113151719122
-    private String Titular;
-    private int saldo;
-    private int saldoDefecto=0;
+    private String iban; //ES1234567891113151719122
+    private String titular;
+    private int saldo=0;
+    private int[] historialMovimientos;
+    private int avisarHacienda=3000;
+    private int saldoNegativo=-50;
+
+    public CuentaBancaria(String iban, String titular) {
+        iban = iban.toUpperCase();
+        if (iban.charAt(0) >= 65 && iban.charAt(0) <= 90) {
+            if (iban.charAt(1) >= 65 && iban.charAt(1) <= 90) {
+                this.iban = iban;
+            } else {
+                System.out.println("IBAN incorrectinto");
+            }
+        } else {
+            System.out.println("IBAN incorrecto");
+        }
+        this.titular = titular;
+    }
+
+    public int ingreso(int ingreso) {
+        if (ingreso > 0) {
+            saldo += ingreso;
+            setHistorialMovimientos(ingreso);
+        }
+        return saldo;
+    }
+
+    public int retirada(int dineroRetirar) {
+        if (dineroRetirar > 0) {
+            saldo -= dineroRetirar;
+            setHistorialMovimientos(-dineroRetirar);
+        }
+        return saldo;
+    }
+
+    private void setHistorialMovimientos(int operacion) {
+        int posicion = historialMovimientos.length - 1;
+        if (operacion>avisarHacienda){
+            System.out.println("AVISO: Notificar a hacienda");
+        } else if (operacion<saldoNegativo){
+            System.out.println("AVISO: Saldo negativo");
+        }
+        historialMovimientos[posicion] = operacion;
+    }
+
 }
