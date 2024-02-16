8b47568f360a378f79378ef7fe273a874d94beb6
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2024-02-16 10:32:10 +0100

    Ej 3 Polimorfismo

--- Tema4/POO4/EJ3/Main.java
+++ Tema4/POO4/EJ3/Main.java
@@ -2,7 +2,6 @@ package POO4.EJ3;
 
 import POO3EJ2.*;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -21,12 +20,22 @@ public class Main {
         boolean condition=true;
         Random random = new Random();
         while (condition){
-            int randomNum= random.nextInt(0,4);
+            int randomNum= random.nextInt(0,5);
             Animal animal = randomAnimal(randomNum);
-            System.out.println(animal);
-
-            if (random.nextInt(1,100)>95){
-
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.err.println();
+            }
+            if (random.nextInt(1,100)<95){
+                switch (randomNum){
+                    case 0->((Pinguino) animal).hacerRuido();
+                    case 1->((Mono) animal).hacerRuido();
+                    case 2->((Perro) animal).hacerRuido();
+                    case 3->((Gato)animal).hacerRuido();
+                    case 4->((Oso) animal).hacerRuido();
+                }
+                System.out.println("Se va a descansar");
             }else {
                 System.out.println("El animal ataca, hay que huir");
                 condition=false;
