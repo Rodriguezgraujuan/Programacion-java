cc3f9ffc3ec66e71297e690dc4e5d5954028eb5e
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2024-04-11 11:53:40 +0200

    ejercicio 3 Colecciones

--- Colecciones/Ejercicio3/Banco.java
+++ Colecciones/Ejercicio3/Banco.java
@@ -17,12 +17,28 @@ public class Banco {
                 """);
     }
 
-    public static Usuario anadirUsuario() {
+    public static boolean checkUsuarioExiste(List<Usuario> banco, String dni){
+        boolean condition =false;
+        for (int i=0;i<banco.size();i++){
+            if (banco.get(i).getDni().equals(dni)){
+                condition=true;
+            }
+        }
+        return condition;
+    }
+
+    public static void anadirUsuario(List<Usuario> banco) {
         System.out.println("Introduce el dni, nombre, edad");
+        in.nextLine();
         String dni = in.nextLine();
-        String nombre = in.nextLine();
-        int edad = in.nextInt();
-        return new Usuario(nombre, edad, dni);
+        if (!checkUsuarioExiste(banco, dni)) {
+            String nombre = in.nextLine();
+            int edad = in.nextInt();
+            banco.add(new Usuario(nombre, edad, dni));
+            System.out.println("Usuario añadido");
+        }else {
+            System.out.println("Usuario existente");
+        }
     }
 
     public static void main(String[] args) {
@@ -30,27 +46,38 @@ public class Banco {
 
         boolean condition = true;
         while (condition) {
+            menu();
             int option = in.nextInt();
 
             switch (option) {
                 case 1:
-                    banco.add(anadirUsuario());
-                    System.out.println("Usuario añadido a la cola");
+                    anadirUsuario(banco);
                     break;
                 case 2:
                     if (!banco.isEmpty()) {
                         System.out.println("Usuario eliminado");
                         banco.remove(0);
+                    } else {
+                        System.out.println("Cola vacia");
                     }
                     break;
                 case 3:
-
+                    if (!banco.isEmpty()){
+                        System.out.println("Introduce la posición");
+                        int position = in.nextInt();
+                        banco.remove(position);
+                    }else{
+                        System.out.println("Cola vacia");
+                    }
                     break;
                 case 4:
                     condition = false;
                     break;
                 default:
                     System.out.println("Valor incorrecto");
+            }
+            if (condition) {
+                System.out.println(banco);
             }
         }
     }
--- Colecciones/Ejercicio3/Usuario.java
+++ Colecciones/Ejercicio3/Usuario.java
@@ -2,13 +2,34 @@ package Ejercicio3;
 
 public class Usuario {
 
-    private String nombre;
-    private int edad;
-    private String dni;
+    private final String nombre;
+    private final int edad;
+    private final String dni;
 
     public Usuario(String nombre, int edad, String dni) {
         this.nombre = nombre;
         this.edad = edad;
         this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", dni='" + dni + '\'' +
+                '}';
     }
 }
