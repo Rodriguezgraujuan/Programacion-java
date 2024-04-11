4c5ac457f6f219711b7c8a9a2812fe044f3d771a
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2024-04-11 12:21:44 +0200

    ejercicio 4 mitad Colecciones

--- Colecciones/Ejercicio1/ListaCompra.java
+++ Colecciones/Ejercicio1/ListaCompra.java
@@ -10,8 +10,27 @@ public class ListaCompra {
     public static String seleccionarObjeto(){
         return in.nextLine();
     }
+
+    public static void menu(){
+        System.out.println("""
+                1. Añadir otra lista
+                2. Mostrar productos existentes
+                3. Productos que faltan añadir
+                4. salir
+                """);
+    }
+
+    public static void addProductosNuevoCarro(Set<String> listaCompra){
+        System.out.println("Cuantos productos");
+        int cantidad= in.nextInt();
+        for (int i=0;i>cantidad;i++){
+            listaCompra.add(in.nextLine());
+        }
+    }
+
     public static void main(String[] args) {
         Set<String> listaCompra= new HashSet<>();
+
         String producto= seleccionarObjeto();
         listaCompra.add(producto);
         for (int i=0;i<3;i++){
@@ -23,5 +42,29 @@ public class ListaCompra {
             }
         }
         System.out.println(listaCompra);
+
+        Set<String> carroCompra= new HashSet<>();
+        Set<String> restantes=new HashSet<>();
+        boolean condition = true;
+        while (condition){
+            menu();
+            int option=in.nextInt();
+            switch (option){
+                case 1:
+                    addProductosNuevoCarro(carroCompra);
+                    break;
+                case 2:
+                    System.out.println(listaCompra);
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+                    condition=false;
+                    break;
+                default:
+                    System.out.println("Valor incorrecto");
+            }
+        }
     }
 }
