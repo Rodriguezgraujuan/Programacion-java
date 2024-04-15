94704d649a195d7a76d8a6cf1a6e377012812e7f
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2024-04-15 10:16:41 +0200

    ejercicio 4 Colecciones

--- Colecciones/Ejercicio1/ListaCompra.java
+++ Colecciones/Ejercicio1/ListaCompra.java
@@ -23,14 +23,21 @@ public class ListaCompra {
     public static void addProductosNuevoCarro(Set<String> listaCompra){
         System.out.println("Cuantos productos");
         int cantidad= in.nextInt();
-        for (int i=0;i>cantidad;i++){
+        in.nextLine();
+        for (int i=0;i<cantidad;i++){
             listaCompra.add(in.nextLine());
         }
+    }
+
+    public static void elementosRestantes(Set<String> restantes, Set<String> listaCompra){
+        restantes.removeAll(listaCompra);
+        System.out.println(restantes);
     }
 
     public static void main(String[] args) {
         Set<String> listaCompra= new HashSet<>();
 
+        System.out.println("Empieza con la lista de la compra:");
         String producto= seleccionarObjeto();
         listaCompra.add(producto);
         for (int i=0;i<3;i++){
@@ -44,7 +51,6 @@ public class ListaCompra {
         System.out.println(listaCompra);
 
         Set<String> carroCompra= new HashSet<>();
-        Set<String> restantes=new HashSet<>();
         boolean condition = true;
         while (condition){
             menu();
@@ -55,9 +61,11 @@ public class ListaCompra {
                     break;
                 case 2:
                     System.out.println(listaCompra);
+                    System.out.println(carroCompra);
                     break;
                 case 3:
-
+                    Set<String> restantes=new HashSet<>(carroCompra);
+                    elementosRestantes(restantes, listaCompra);
                     break;
                 case 4:
                     condition=false;
