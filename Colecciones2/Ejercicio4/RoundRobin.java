6cf300648b6a0b727840e303a39fccda4a567b70
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2024-04-22 16:57:02 +0200

    Conjuntos 2 ejercicio 4

--- Colecciones2/Ejercicio4/RoundRobin.java
+++ Colecciones2/Ejercicio4/RoundRobin.java
@@ -7,25 +7,31 @@ import java.util.Random;
 public class RoundRobin {
 
     static Random random=new Random();
-
-    public static Proceso generarProcesoAleatorio(List<Proceso> listasProcesos){
-        Proceso proceso = new Proceso("", random.nextInt(1,4));
-        return proceso;
+    public static Proceso generarProcesoAleatorio(List<Integer> numeros) {
+        String name = "p"+numeros.size();
+        numeros.add(numeros.size());
+        return new Proceso(name, random.nextInt(1, 4));
     }
-
     public static void main(String[] args) {
         List<Proceso> listasProcesos = new ArrayList<>();
-        listasProcesos.add(new Proceso("p1", 2));
-        listasProcesos.add(new Proceso("p2", 1));
-        generarProcesoAleatorio(listasProcesos);
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(0);
+
+        int vecesPasadas=0;
+        listasProcesos.add(new Proceso("p0", random.nextInt(3,4)));
         while (!(listasProcesos.isEmpty())){
             listasProcesos.forEach(proceso->proceso.setSegundo(proceso.getSegundo()-0.2));
             try {
-                Thread.sleep(2000);
+                Thread.sleep(0200);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
+            vecesPasadas++;
+            if (vecesPasadas==10){
+                vecesPasadas=0;
+                listasProcesos.add(generarProcesoAleatorio(numeros));
+            }
+            listasProcesos.removeIf(proceso -> proceso.getSegundo() < 0.2);
             listasProcesos.forEach(System.out::println);
         }
     }
--- Tema2/Tema2/comprob.java
+++ Tema2/Tema2/comprob.java
@@ -3,5 +3,7 @@ package Tema2;
 import java.util.Scanner;
 
 public class comprob {
-
+    public static void main(String[] args) {
+        
+    }
 }
